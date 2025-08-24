3a3ffcc3434b24a9bd9f32ef9930cc9b7a4be72e
urvi213 <urvimaheshwari1234@gmail.com>
2025-08-24 09:59:57 +0100

    Update data preprocessing hw.py

--- data preprocessing hw.py
+++ data preprocessing hw.py
@@ -14,3 +14,25 @@ X = screen_time[["Age","Gender","Avg_Daily_Screen_Time_hr","Primary_Device","Edu
 y = screen_time["Exceeded_Recommended_Limit"]
 print(X)
 print(y)
+
+# encoding
+encoded_X = pd.get_dummies(X,columns=["Gender","Primary_Device","Health_Impacts","Urban_or_Rural"],dtype=int)
+print(encoded_X)
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+encoded_y = le.fit_transform(y)
+print(encoded_y)
+
+# # scaling
+# from sklearn.preprocessing import MinMaxScaler
+# mms = MinMaxScaler()
+# encoded_X[["Age"]] = mms.fit_transform(encoded_X[["Age"]])
+# print(encoded_X[["Age"]])
+
+# split into train and test set
+from sklearn.model_selection import train_test_split
+Xtrain, Xtest, ytrain, ytest = train_test_split(encoded_X,encoded_y,test_size=0.1)
+print(Xtrain)
+print(Xtest)
+print(ytrain)
+print(ytest)
